9ba21d4f381b5ba95e9668b945d97406ded8e9c0
Sammyjoebrown <sammyjoebrownme@gmail.com>
2025-01-14 01:12:22 +1000

    Completed front-end interfaces 1

--- Unit1.pas
+++ Unit1.pas
@@ -25,6 +25,23 @@ type
     Button1: TButton;
     Label2: TLabel;
     Shape2: TShape;
+    Label4: TLabel;
+    Edit2: TEdit;
+    Shape3: TShape;
+    Label5: TLabel;
+    Button2: TButton;
+    Button3: TButton;
+    Edit3: TEdit;
+    Label6: TLabel;
+    Shape4: TShape;
+    Label7: TLabel;
+    Label8: TLabel;
+    Edit4: TEdit;
+    Button4: TButton;
+    Button5: TButton;
+    RadioButton3: TRadioButton;
+    RadioButton4: TRadioButton;
+    Label9: TLabel;
   private
     { Private declarations }
   public
